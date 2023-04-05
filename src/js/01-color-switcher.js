3a946504e87d46b4f9299cd1fc9cca9223c3a209
SSSerg05 <ssserg05@gmail.com>
2023-04-05 17:50:33 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
 
+function onStart() { 
+  const body = document.body;
+  const timer = 
+
+  body.addEventListener('click', (event) => { 
+    event.preventDefault();
+    
+    if (event.target.dataset.action === 'start') {
+      body.style.backgroundColor = getRandomHexColor();
+    } else if ((event.target.dataset.action === 'stop') )
+      // stopTimer()
+  })
+}
+
+onStart()
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
