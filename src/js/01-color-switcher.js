1ca6e2e01ce86bd704e07cbe906d898373c6a19c
SSSerg05 <ssserg05@gmail.com>
2023-04-06 09:18:19 +0300

    fix color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,19 +1,27 @@
 
-function onStart() { 
+function init() { 
   const body = document.body;
   const timer = 
 
   body.addEventListener('click', (event) => { 
     event.preventDefault();
-    
+    let timerId;
     if (event.target.dataset.action === 'start') {
-      body.style.backgroundColor = getRandomHexColor();
-    } else if ((event.target.dataset.action === 'stop') )
-      // stopTimer()
+      timerId = setInterval(onStart, 1000, body);
+    } else if ((event.target.dataset.action === 'stop'))
+      timerId = clearInterval();
   })
 }
 
-onStart()
+init()
+
+function onStart(teg) { 
+  teg.style.backgroundColor = getRandomHexColor();
+}
+function onStop() { 
+  
+}
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
