01a71070caa0165f76c4ff10ed956565b005a2c2
SSSerg05 <119066393+SSSerg05@users.noreply.github.com>
2023-04-07 14:02:54 +0000

    Deploying to gh-pages from @ SSSerg05/goit-js-hw-09@7da108b003578f4278838185e17a231761db2e41 🚀

--- 01-color-switcher.1efb6a16.js
+++
@@ -1,2 +0,0 @@
-function t(t){t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}!function(){const a=document.body;a.addEventListener("click",(e=>{let n;e.preventDefault(),"start"===e.target.dataset.action?n=setInterval(t,1e3,a):"stop"===e.target.dataset.action&&(n=clearInterval())}))}();
-//# sourceMappingURL=01-color-switcher.1efb6a16.js.map
