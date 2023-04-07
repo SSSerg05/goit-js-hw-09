7da108b003578f4278838185e17a231761db2e41
SSSerg05 <ssserg05@gmail.com>
2023-04-07 17:02:31 +0300

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,15 +1,24 @@
 
 function init() { 
   const body = document.body;
-  const timer = 
+  let timerId = null;
 
   body.addEventListener('click', (event) => { 
-    event.preventDefault();
-    let timerId;
-    if (event.target.dataset.action === 'start') {
-      timerId = setInterval(onStart, 1000, body);
-    } else if ((event.target.dataset.action === 'stop'))
-      timerId = clearInterval();
+
+    if ('start' in event.target.dataset) {
+
+      if (!timerId) {
+        timerId = setInterval(onStart, 1000, body);
+      }
+
+    } else if ('stop' in event.target.dataset) {
+
+      if (timerId) {
+        clearInterval(timerId);
+        timerId = null;
+      }
+
+    }
   })
 }
 
@@ -18,10 +27,6 @@ init()
 function onStart(teg) { 
   teg.style.backgroundColor = getRandomHexColor();
 }
-function onStop() { 
-  
-}
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
