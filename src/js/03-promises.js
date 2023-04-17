6ba4e28c3e22fe4c57d982cf9706490955c13969
SSSerg05 <ssserg05@gmail.com>
2023-04-17 11:45:06 +0300

    start 9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,6 @@
+// all modules
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -5,4 +8,27 @@ function createPromise(position, delay) {
   } else {
     // Reject
   }
+}
+
+const form = document.querySelector('.form');
+const refs = { 
+  delay: form.elements.delay,
+  step: form.elements.step,
+  amount: form.elements.amount,
+}
+form.addEventListener("submit", onSubmitForm)
+
+function onSubmitForm(event) { 
+  for (const position in refs.amount) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`)
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+  
 }
