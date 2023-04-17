8a6267426f41adc8243a3f945f701ba1503fb569
SSSerg05 <ssserg05@gmail.com>
2023-04-17 18:01:04 +0300

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,17 +2,14 @@
 import Notiflix from 'notiflix';
 
 function createPromise(position, delay) {
+  ()
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`Success ${shouldResolve}! Value ${position} passed to resolve function`);
-        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`))
       } else {
-        reject(`Error ${shouldResolve}! Error ${position} passed to reject function`);
-        Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`)
-         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject(Notiflix.Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`))
       }
     }, delay);
   })
@@ -20,18 +17,16 @@ function createPromise(position, delay) {
 
 const form = document.querySelector('.form');
 const refs = { 
-  delay: form.elements.delay.value,
-  step: form.elements.step.value,
-  amount: form.elements.amount.value,
+  delay: form.elements.delay,
+  step: form.elements.step,
+  amount: form.elements.amount,
 }
 form.addEventListener("submit", onSubmitForm)
-console.log(refs.delay, refs.step, refs.amount);
-console.log(refs);
 
 function onSubmitForm(event) { 
-  for (const i = 0; i < refs.amount; i++) {
-    createPromise(i, delay)
-    console.log(i, delay);
+  for (const i = 0; i < Number(refs.amount.value); i++) {
+    createPromise(i, Number(delay.value))
+    console.log(i, Number(delay.value));
   }
   console.log(refs);
 
